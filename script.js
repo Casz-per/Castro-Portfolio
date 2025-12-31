076ee7e4450ac174690d8f595655a3cd2f3713c6
Casz-per <castronikov8@gmail.com>
2025-12-31 14:46:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openProject(name) {
+  alert("Project Selected: " + name + "\n\nYou can replace this with a modal or link later!");
+}
+
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
